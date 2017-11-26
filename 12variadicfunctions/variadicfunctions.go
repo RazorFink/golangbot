7abab81894615e855e5e96825e33af19a3fda681
Matt Howe <mhowe@bates.edu>
2017-11-26 15:55:16 -0500

    more practice examples

--- 12variadicfunctions/variadicfunctions.go
+++ 12variadicfunctions/variadicfunctions.go
@@ -18,7 +18,20 @@ func find(num int, nums ...int) {
 		fmt.Printf("%d not found\n", num)
 	}
 }
+
+func change(s ...string) {
+	s = append(s, "playground")
+	s[0] = "Go"
+	fmt.Println(s)
+}
+
 func main() {
 	find(1, 2, 3, 4, 5, 6)
 	find(2, 3, 4, 5, 6, 2, 7)
+	s := []int{8, 7, 6, 5, 4, 3, 7}
+	find(7, s...)
+
+	welcome := []string{"hello", "world"}
+	change(welcome...)
+	fmt.Println(welcome)
 }
